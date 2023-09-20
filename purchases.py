ca2e5856f0c5f13589e565a8212c6309aac4e8d9
CClarkaau <clarkaau@bc.edu>
2023-09-20 13:22:41 -0400

    purchase.py0

---
+++ purchases.py
@@ -0,0 +1,50 @@
+#decsription: purchases.py
+#author: Clark.Colin
+#date: 9.18.23
+
+#b. add tax function
+def add_tax (lst,sls_tx):
+    updated_list = lst.copy()
+    i = 0
+    while i < len(updated_list):
+        updated_list[i] += (sls_tx * updated_list[i])
+        i += 1
+        input(updated_list)
+    return updated_list
+'''
+MAIN
+'''
+#a. Get input and create lists
+number_of_purchases = int(input("Number of purchases: "))
+sales_tax = float(input("Sales tax: "))
+#Initialize lists
+customer_names = []
+item_costs = []
+#initialize the dictionary
+reciept_dict = {}
+while(number_of_purchases) > 0:
+    customer_names.append(input("Customer: "))
+    item_costs.append(float(input("Cost: ")))
+    number_of_purchases -= 1
+#c. use add_tax to apply sales tax to the costs in cost list
+updated_list = add_tax(item_costs, sales_tax)
+print(item_costs, customer_names, updated_list)
+#d. define an empty dictionary with customer : cost
+for i in range(len(customer_names)):
+    name = customer_names[i]
+    price = updated_list[i]
+    if name in reciept_dict:
+        reciept_dict[name] += price
+    else:
+        reciept_dict[name] = price
+
+
+    """
+power point dictionary example
+"""
+#Outputs
+print(reciept_dict)
+
+""" 
+and printing out the dictionary at the end gives coordinates 0: {Bob : 3.3}
+"""
